17855e0b51bbf4a333b09bf021f03fca21255d81
Jemora02 <jemora02@knights.ucf.edu>
2021-09-30 13:21:15 -0400

    Animation commit

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,56 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Music : MonoBehaviour
+public class Controller : MonoBehaviour
 {
-    public AudioClip musicClipOne;
+public AudioClip musicClipOne;
 
-    public AudioClip musicClipTwo;
+public AudioClip musicClipTwo;
 
-    public AudioSource musicSource;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public AudioSource musicSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
+Animator anim;
+
+void Start()
+
+{
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
+     if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
+
     }
 }
